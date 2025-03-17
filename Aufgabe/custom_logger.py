a6200ae239edde3bfb8c45d7ee594382911d5227
schotterhaufn <98356337+schotterhaufn@users.noreply.github.com>
2025-03-17 13:49:47 +0100

    adds formatter for logging

--- Aufgabe/custom_logger.py
+++ Aufgabe/custom_logger.py
@@ -4,13 +4,17 @@ import logging
 logger = logging.getLogger('my_logger')
 logger.setLevel(logging.DEBUG)
 
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
 # Create a file handler to write logs to a file
 file_handler = logging.FileHandler('app.log')
 file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
 
 # Create a stream handler to print logs to the console
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)  # You can set the desired log level for console output
+console_handler.setFormatter(formatter)
 
 # Add the handlers to the logger
 logger.addHandler(file_handler)
