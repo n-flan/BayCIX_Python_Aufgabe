b4f2cd70fd3be581c2013c2a185c243d44d1c634
schotterhaufn <98356337+schotterhaufn@users.noreply.github.com>
2025-03-18 08:29:41 +0100

    adds better type hints for smtp in external python script

--- Aufgabe/auth_functions.py
+++ Aufgabe/auth_functions.py
@@ -1,35 +1,34 @@
 from custom_logger import logger
+import smtplib
 
-
-
-def auth_plain(smtp):    
+def auth_plain(smtp: smtplib.SMTP):    
     smtp.connect()
     logger.info('Testing method PLAIN')
     try:
         smtp.auth('PLAIN', smtp.auth_plain)
         logger.info('Testing method PLAIN succeeded')
-    except:
-        logger.error('Testing method PLAIN failed.')
+    except Exception as e:
+        logger.error(f'Testing method PLAIN failed. Exception: {e}')
     smtp.close()
 
-def auth_login(smtp):
+def auth_login(smtp: smtplib.SMTP):
     smtp.connect()
     logger.info('Testing method LOGIN')
     try:
         smtp.auth('LOGIN', smtp.auth_login)
         logger.info('Testing method LOGIN succeeded')
-    except:
-        logger.error('Testing method LOGIN failed.')
+    except Exception as e:
+        logger.error(f'Testing method LOGIN failed. Exception: {e}')
     smtp.close()
 
-def auth_cram_md5(smtp):
+def auth_cram_md5(smtp: smtplib.SMTP):
     smtp.connect()
     logger.info('Testing method CRAM-MD5')
     try:
         smtp.auth('CRAM-MD5', smtp.auth_cram_md5)
         logger.info('Testing method CRAM-MD5 succeeded')
-    except:
-        logger.error('Testing method CRAM-MD5 failed.')
+    except Exception as e:
+        logger.error(f'Testing method CRAM-MD5 failed. Exception: {e}')
     smtp.close()
 
 def auth_digest_md5():
